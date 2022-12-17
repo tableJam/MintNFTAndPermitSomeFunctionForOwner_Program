00c1f1e5b054c53059ba1cda462a87b4ce2aaf4b
tableJam <wtswtyj2020@gmail.com>
2022-12-18 05:13:01 +0900

    tokenbank

--- contracts/TokenBank.sol
+++ contracts/TokenBank.sol
@@ -30,7 +30,26 @@ contract TokenBank{
         name = name_;
         symbol = symbol_;
         owner = msg.sender;
-        _balances[owner] = bankTotakDeposit;
+        _balances[owner] = totalSpply;
+    }
+
+    /// @dev return token name
+    function getname() public view returns (string memory){
+        return name;
+    }
+    /// @dev return token symbol
+    function getsymbol() public view returns (string memory) {
+        return symbol;
+    }
+
+    /// @dev return token total supply
+    function gettotalsupply() public pure returns (uint256) {
+        return totalSpply;
+    } 
+
+    /// @dev return address => token
+       function blanceOf(address account) public view returns(uint256) {
+        return _balances[account];
     }
 
 }
